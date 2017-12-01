cbfa67c9ab057c3d4692fa33123772e2d887b4f2
tuyungang <tyg362153368@126.com>
2017-12-01 14:35:17 +0800

    加了注释，规范化了下

--- ini_config.h
+++ ini_config.h
@@ -1,3 +1,17 @@
+/* *******************************************************
+ * Call Center On Demand Product Series
+ * Copyright (C) 2017 HonDa(Guangzhou.) Technology Ltd., Co.
+ * All right reserved
+ *
+ * @file ini_config.h
+ * @brief 
+ * @author tuyungang
+ * @version v1.0
+ * @date 2017-12-01
+ * 
+ * TODO: 读写ini配置文件
+ * 
+ * *******************************************************/
 #ifndef _INI_CONFIG_H
 #define _INI_CONFIG_H
 
@@ -19,9 +33,19 @@ typedef struct
 }sAgentConfigInfo, *psAgentConfigInfo;
 */
 
-
 bool InitIniConfig();
 void trim(char *str);
+
+/* --------------------------------------------------------------------------*/
+/**
+ * @brief GetIniKeyString 
+ * @description 获取字符串值，按section和key值
+ *
+ * @param section
+ * @param key
+ * @param vaule
+ */
+/* ----------------------------------------------------------------------------*/
 void GetIniKeyString(const char *section, const char *key, char *vaule);
 void GetIniKeyInt(const char *section, const char *key, int vaule);
 void GetIniKeyLong(const char *section, const char *key, long vaule);
